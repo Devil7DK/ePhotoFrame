26fb42fdd2ffa4d84f3fb8b2fed2c540fa62514d
Devil7DK <dineshthangavel47@gmail.com>
2026-05-25 20:59:56 +0530

    Use single file plugin with vite to generate index.html with all code inlined

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
   css: {
     transformer: "lightningcss",
   },
-  plugins: [preact()],
+  plugins: [preact(), viteSingleFile()],
 });
